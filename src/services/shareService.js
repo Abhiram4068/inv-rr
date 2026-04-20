8f8fb3ec0095698641b8161bd0c97738dca26f0a
Abhiram4068 <abhiram4068@gmail.com>
2026-04-20 14:25:20 +0530

    feat: implement file scheduling service and integrate dynamic file picker in ScheduleMail page

--- src/services/shareService.js
+++ src/services/shareService.js
@@ -3,3 +3,7 @@ import api from "../api/axios";
 // POST /files/:id/share/
 export const shareFile = (fileId, shareData) =>
   api.post(`/files/${fileId}/share/`, shareData);
+
+// POST /files/:id/share/schedule/
+export const scheduleShareFile = (fileId, shareData) =>
+  api.post(`/files/${fileId}/share/schedule/`, shareData);
